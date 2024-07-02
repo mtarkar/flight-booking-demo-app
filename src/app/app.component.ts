ce69cebf3be0daf1e144052d9007f6282f301056
mithilesh <mtarkar.consulting@gmail.com>
2024-07-02 12:54:54 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'flight-booking-demo-app';
+
+
+    locations = [
+      'New York',
+      'Mumbai',
+      'Chennai',
+      'Kolkata',
+      'New Delhi'
+    ]
+
+
+  errors = {
+    name: 'Name is Required',
+    email: 'Email is Required',
+    to: 'Date is Required',
+    from: 'Date is Required'
+  }
+
+
+
+  form = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required]),
+    to: new FormControl(null, [Validators.required]),
+    from: new FormControl(null, [Validators.required]),
+    date: new FormControl(null, [Validators.required]),
+
+  })
+
+  get name() {
+    return this.form.get('name')
+  }
+  get email() {
+    return this.form.get('email')
+  }
+  get to() {
+    return this.form.get('to')
+  }
+  get from() {
+    return this.form.get('from')
+  }
+  get date() {
+    return this.form.get('date')
+  }
+
+  submitData() {
+    console.log(
+      this.form.value
+    )
+  }
+
+
+
+
+
 }
